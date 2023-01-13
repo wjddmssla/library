3a200af937acfafd29287bb8a5089be492b2d280
wjddmssla <pakrose0108@naver.com>
2023-01-13 21:48:16 +0900

    library project8

--- library/src/main/java/com/korit/library/service/BookService.java
+++ library/src/main/java/com/korit/library/service/BookService.java
@@ -1,12 +1,17 @@
 package com.korit.library.service;
 
+import com.korit.library.exception.CustomValidationException;
 import com.korit.library.repository.BookRepository;
 import com.korit.library.web.dto.BookMstDto;
+import com.korit.library.web.dto.BookReqDto;
+import com.korit.library.web.dto.CategoryDto;
 import com.korit.library.web.dto.SearchReqDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class BookService {
@@ -17,5 +22,28 @@ public class BookService {
     public List<BookMstDto> searchBook(SearchReqDto searchReqDto) {
         searchReqDto.setIndex();
         return bookRepository.searchBook(searchReqDto);
+    }
+
+    public List<CategoryDto> getCategories() {
+        return bookRepository.findAllCategory();
+    }
+
+    public void registerBook(BookReqDto bookReqDto){
+        duplicateBookCode(bookReqDto.getBookCode());
+        bookRepository.saveBook(bookReqDto);
+    }
+
+    private void duplicateBookCode(String bookCode) {
+        BookMstDto bookMstDto = bookRepository.findBookByBookCode(bookCode);
+        if(bookMstDto != null) {
+            Map<String, String> errorMap = new HashMap<>();
+            errorMap.put("bookCode", "이미 존재하는 도서코드입니다.");
+
+            throw new CustomValidationException(errorMap);
+        }
+    }
+
+    public void modifyBook(BookReqDto bookReqDto) {
+        bookRepository.updateBookByBookCode(bookReqDto);
     }
 }
--- library/src/main/java/com/korit/library/web/api/admin/BookApi.java
+++ library/src/main/java/com/korit/library/web/api/admin/BookApi.java
@@ -3,17 +3,13 @@ package com.korit.library.web.api.admin;
 import com.korit.library.aop.annotation.ParamsAspect;
 import com.korit.library.aop.annotation.ValidAspect;
 import com.korit.library.service.BookService;
-import com.korit.library.web.dto.BookMstDto;
-import com.korit.library.web.dto.CMRespDto;
-import com.korit.library.web.dto.SearchReqDto;
+import com.korit.library.web.dto.*;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -34,5 +30,32 @@ public class BookApi {
                 .ok()
                 .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", bookService.searchBook(searchReqDto)));
 
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<CMRespDto<List<CategoryDto>>> getCategories() {
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", bookService.getCategories()));
+    }
+
+    @ParamsAspect
+    @ValidAspect
+    @PostMapping("/book")
+    public ResponseEntity<CMRespDto<?>> registerBook(@Valid @RequestBody BookReqDto bookReqDto, BindingResult bindingResult) {
+        bookService.registerBook(bookReqDto);
+        return ResponseEntity
+                .created(null)
+                .body(new CMRespDto<>(HttpStatus.CREATED.value(), "Successfully", true));
+    }
+
+    @ParamsAspect
+    @ValidAspect
+    @PutMapping("/book/{bookCode}")
+    public ResponseEntity<CMRespDto<?>> modifyBook(@PathVariable String bookCode, @Valid @RequestBody BookReqDto bookReqDto, BindingResult bindingResult) {
+        bookService.modifyBook(bookReqDto);
+        return ResponseEntity
+                .created(null)
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
     }
 }
