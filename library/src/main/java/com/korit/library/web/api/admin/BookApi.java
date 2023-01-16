73176fd7429a5349e97c8713c66228579cd0e604
wjddmssla <pakrose0108@naver.com>
2023-01-16 21:51:21 +0900

    library project9

--- library/src/main/java/com/korit/library/web/api/admin/BookApi.java
+++ library/src/main/java/com/korit/library/web/api/admin/BookApi.java
@@ -5,12 +5,17 @@ import com.korit.library.aop.annotation.ValidAspect;
 import com.korit.library.service.BookService;
 import com.korit.library.web.dto.*;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import org.apache.coyote.Response;
+import org.apache.ibatis.annotations.CacheNamespaceRef;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.annotation.MultipartConfig;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -58,4 +63,33 @@ public class BookApi {
                 .created(null)
                 .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
     }
+
+    @ParamsAspect
+    @ValidAspect
+    @PatchMapping("/book/{bookCode}")
+    public ResponseEntity<CMRespDto<?>> maintainModifyBook(@PathVariable String bookCode, @Valid @RequestBody BookReqDto bookReqDto, BindingResult bindingResult) {
+        bookService.modifyBook(bookReqDto);
+        return ResponseEntity
+                .created(null)
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+    @ParamsAspect
+    @DeleteMapping("/book/{bookCode}")
+    public ResponseEntity<CMRespDto<?>> removeBook(@PathVariable String bookCode) {
+        bookService.removeBook(bookCode);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+    @ParamsAspect
+    @PostMapping("/book/{bookCode}/images")
+    public ResponseEntity<CMRespDto<?>> registerBookImg(@PathVariable String bookCode, @RequestPart List<MultipartFile> files) {
+        bookService.registerBookImage(bookCode, files);
+        return ResponseEntity.ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+
+    }
+
 }
