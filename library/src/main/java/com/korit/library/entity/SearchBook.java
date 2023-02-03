658580c4c6944533333098087561081f899bf5c3
wjddmssla <pakrose0108@naver.com>
2023-02-03 21:51:16 +0900

    library project 20

---
+++ library/src/main/java/com/korit/library/entity/SearchBook.java
@@ -0,0 +1,17 @@
+package com.korit.library.entity;
+
+import java.time.LocalDate;
+
+public class SearchBook {
+    private int bookId;
+    private String bookCode;
+    private String bookName;
+    private String author;
+    private String publisher;
+    private String publicationDate;
+    private String category;
+    private String saveName;
+    private int rentalDtlId;
+    private LocalDate returnDate;
+    private int userId;
+}
---
+++ library/src/main/java/com/korit/library/repository/SearchRepository.java
@@ -0,0 +1,8 @@
+package com.korit.library.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class SearchRepository {
+
+}
