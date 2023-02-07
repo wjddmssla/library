440ded683a58e3b7cf851fdfb21e84b2aea52a4a
wjddmssla <pakrose0108@naver.com>
2023-02-07 21:56:07 +0900

    library project 22

--- library/src/main/resources/static/js/search/search.js
+++ library/src/main/resources/static/js/search/search.js
@@ -1,10 +1,17 @@
 window.onload = () => {
+    HeaderService.getInstance().loadHeader();
+
     SearchService.getInstance().clearBookList();
     SearchService.getInstance().loadSearchBooks();
     SearchService.getInstance().loadCategories();
+    SearchService.getInstance().setMaxPage();
 
     ComponentEvent.getInstance().addClickEventCategoryCheckboxs();
+    ComponentEvent.getInstance().addScrollEventPaging();
+    ComponentEvent.getInstance().addClickEventSearchButton();
 }
+
+let maxPage = 0;
 
 const searchObj = {
     page: 1,
@@ -64,7 +71,7 @@ class SearchApi {
 
     searchBook() {
         let responseData = null;
-        
+
         $.ajax({
             async: false,
             type: "get",
@@ -92,10 +99,17 @@ class SearchService {
         return this.#instance;
     }
 
+    setMaxPage() {
+        const totalCount = SearchApi.getInstance().getTotalCount();
+        maxPage = totalCount % 10 == 0 
+            ? totalCount / 10 
+            : Math.floor(totalCount/10) + 1;
+    }
+
     loadCategories() {
         const categoryList = document.querySelector(".category-list");
         categoryList.innerHTML = ``;
-        
+
         const responseData = SearchApi.getInstance().getCategories();
         responseData.forEach(categoryObj => {
             categoryList.innerHTML += `
@@ -116,23 +130,23 @@ class SearchService {
         const responseData = SearchApi.getInstance().searchBook();
         const contentFlex = document.querySelector(".content-flex");
 
-        responseData.forEach(data=> {
+        responseData.forEach(data => {
             contentFlex.innerHTML += `
                 <div class="info-container">
                     <div class="book-desc">
                         <div class="img-container">
-                            <img src="http://127.0.0.1/image/book/${data.saveName != null ? data.saveName: "no_img.png"}" class="book-img">
+                            <img src="http://127.0.0.1:8000/image/book/${data.saveName != null ? data.saveName : "no_image.png"}" class="book-img">
                         </div>
                         <div class="like-info"><i class="fa-regular fa-thumbs-up"></i> <span class="like-count">${data.likeCount != null ? data.likeCount : 0}</span></div>
                     </div>
-            
+                    
                     <div class="book-info">
                         <div class="book-code">${data.bookCode}</div>
-                        <h3 class="book-name">$data.bookName}</h2>
+                        <h3 class="book-name">${data.bookName}</h2>
                         <div class="info-text book-author"><b>저자: </b>${data.author}</div>
                         <div class="info-text book-publisher"><b>출판사: </b>${data.publisher}</div>
                         <div class="info-text book-publicationdate"><b>출판일: </b>${data.publicationDate}</div>
-                        <div class="info-text book-category"><b>카테고리: </b>${data.Category}</div>
+                        <div class="info-text book-category"><b>카테고리: </b>${data.category}</div>
                         <div class="book-buttons">
                             <button type="button" class="rental-button">대여하기</button>
                             <button type="button" class="like-button">추천</button>
@@ -142,6 +156,7 @@ class SearchService {
             `;
         })
     }
+
 }
 
 class ComponentEvent {
@@ -164,8 +179,42 @@ class ComponentEvent {
                     const index = searchObj.categories.indexOf(checkbox.value);
                     searchObj.categories.splice(index, 1);
                 }
-                console.log(searchObj.categories);
+                document.querySelector(".search-button").onclick();
             }
         });
+    }
+
+    addScrollEventPaging() {
+        const html = document.querySelector("html");
+        const body = document.querySelector("body");
+
+        body.onscroll = () => {
+            const scrollPosition = body.offsetHeight - html.clientHeight - html.scrollTop;
+
+            if(scrollPosition < 250 && searchObj.page < maxPage) {
+                searchObj.page++;
+                SearchService.getInstance().loadSearchBooks();
+            }
+        }
+    }
+
+    addClickEventSearchButton() {
+        const searchButton = document.querySelector(".search-button");
+        const searchInput = document.querySelector(".search-input");
+        
+        searchButton.onclick = () => {
+            searchObj.searchValue = searchInput.value;
+            searchObj.page = 1;
+            window.scrollTo(0, 0);
+            SearchService.getInstance().clearBookList();
+            SearchService.getInstance().setMaxPage();
+            SearchService.getInstance().loadSearchBooks();
+        }
+
+        searchInput.onkeyup = () => {
+            if(window.event.keyCode == 13) {
+                searchButton.click();
+            }
+        }
     }
 }
